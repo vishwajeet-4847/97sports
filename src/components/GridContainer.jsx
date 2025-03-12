c6adf8bd22befde0be664b7bce58518363a0a57d
vishwajeet-4847 <exclusivebloh@gmail.com>
2025-03-12 22:25:45 +0530

    new comp edit added

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GridContainer = ({ children }) => {
   return (
-    <div className="grid  grid-cols-3 md:grid-cols-3 gap-4 p-4">
+    <div className="grid  grid-cols-3 md:grid-cols-3 gap-4 p-[15px]">
       {children}
     </div>
   );
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Section = ({ children, className = "" , backgroundImage }) => {
   return (
     <section className={`w-full bg-gray-100 py-8 ${className}`} style={{ backgroundImage: `url(${backgroundImage || ""})` }}>
-      <div className="w-[80%] mx-auto">{children}</div>
+      <div className="w-[95%] mx-auto">{children}</div>
     </section>
   );
 };

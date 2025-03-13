7893ae919f64f5ea7b745238901681468e86d672
BEaANIKET <aniketchaturvedi309@gmail.com>
2025-03-13 17:43:16 +0530

    aniket push

--- src/main.jsx
+++ src/main.jsx
@@ -9,23 +9,22 @@ import SettingHeader from "./components/Roshan/Profile/SettingHeader.jsx";
 import ExchangeHeader from "./components/Roshan/Profile/ExchangeHeader.jsx";
 import { Homescreen } from "./screens/Homescreen.jsx";
 import GameDetailsScreen from "./screens/GameDetailsScreen.jsx";
-
+import { EventDetails } from "./screens/EventDetails.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-     {/* <AuthProvider> */}
-     <Router>
+    {/* <AuthProvider> */}
+    <Router>
       <Routes>
-        <Route path="/" element={<App />} >
-        <Route path="/home" element={<Homescreen />} />
-        <Route path="/settings" element={<SettingHeader/>} />
-        <Route path="/Profile" element={<ExchangeHeader />} />
-        <Route path="/in-play" element={<InPlayScreen/>} />
-        <Route path="/game" element={<GameDetailsScreen/>} />
-        {/* <Route path="/casion/:id" element={<Casino />} /> */}
-         </Route>
+        <Route path="/" element={<App />}>
+          <Route path="/home" element={<Homescreen />} />
+          <Route path="/settings" element={<SettingHeader />} />
+          <Route path="/account" element={<ExchangeHeader />} />
+          <Route path="/in-play" element={<InPlayScreen />} />
+          <Route path="/game" element={<GameDetailsScreen />} />
+          <Route path="/casion/:id" element={<EventDetails />} />
+        </Route>
       </Routes>
-     
     </Router>
     {/* </AuthProvider> */}
   </StrictMode>

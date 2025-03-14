fac6570bbded86f6b42148dea44bcf8a0de28f4c
vishwajeet-4847 <exclusivebloh@gmail.com>
2025-03-14 12:48:03 +0530

    casino live video added

--- src/components/CasinoCard.jsx
+++ src/components/CasinoCard.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { useNavigate } from "react-router";
 
 const CasinoCard = ({ game }) => {
+  
   const navigate = useNavigate();
   
-  
-
+  if (!game) return null;
   return (
     <div
       onClick={() => navigate(`/casino/${game.gmid}`, { state: game })}
       className="relative shadow-lg rounded-md mb-4 cursor-pointer bg-gradient-to-b from-red-600 to-red-800 min-h-[140px] transition-transform duration-200 hover:scale-105 flex flex-col"
     >
       <img
-        src={game.imgPath}
+        src={`https://nd.sprintstaticdata.com/casino-icons/lc/${game.imgpath}`}
         alt={game.gname}
         className="w-full h-[120px] object-cover rounded-t-md"
       />
